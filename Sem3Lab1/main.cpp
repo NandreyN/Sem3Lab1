d17320a336233b7264506cb8c3866f2bfa892145
NandreyN <nandreyn1998@gmail.com>
2017-09-01 16:58:42 +0300

    task2

--- Sem3Lab1/main.cpp
+++ Sem3Lab1/main.cpp
@@ -2,10 +2,16 @@
 1. Написать функцию, которая вычисляет сумму списка аргументов произвольной длины с разными типами элементов списка.
 15:00 01.09.2017
 */
+/*
+2. Написать функцию, которая вычисляет минимальное значение списка
+аргументов произвольной длины с разными типами элементов списка.*/
+
 #include <iostream>
 #include <cstdarg>
 #include <map>
 #include <functional>
+#include <algorithm>
+#include <initializer_list>
 using namespace std;
 
 namespace Task1
@@ -22,6 +28,22 @@ namespace Task1
 	}
 }
 
+namespace Task2
+{
+	double min(double d1, double d2)
+	{
+		return d1 < d2 ? d1 : d2;
+	}
+
+	template<typename ...T>
+	double min(double d1,double d2, T...t)
+	{
+		double m = d1 < d2 ? d1 : d2;
+		return min(m, t...);
+	}
+
+}
+
 template<typename ... Args>
 void executeTask(int number,Args... args)
 {
@@ -30,6 +52,16 @@ void executeTask(int number,Args... args)
 	case 1:
 		cout << "Task1 : " << Task1::sum(args...) << endl;
 		break;
+
+	case 2:
+		if (sizeof...(args) > 0)
+		{
+			cout << "Task2 : " << Task2::min(args...) << endl;	
+		}
+		else
+			cout << "Task2 : invalid param list length" << endl;
+		break;
+
 	default:
 		cout << "Invalid task number , try again" << endl;
 		break;
@@ -39,7 +71,7 @@ void executeTask(int number,Args... args)
 int main()
 {
 	//executeTask(1, 2, 4, 5);
-
+	executeTask(2, 7, 2.5, 1, 0.22, 88,0.1);
 
 	system("pause");
 	return 0;
